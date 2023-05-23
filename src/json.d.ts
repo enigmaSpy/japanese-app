f4b9818834f181ca5ed09e28cc8203f370a65873
enigmaSpy <soczekv18@gmail.com>
2023-05-23 15:04:54 +0200

    added katakanaAlphabet in json

--- src/json.d.ts
+++ src/json.d.ts
@@ -2,4 +2,4 @@ declare module '*.json' {
     const value: any;
     export default value;
 }
-//To poinformuje Twoją aplikację o tym, że pliki z rozszerzeniem .json powinny być traktowane jako moduły.
+//!To poinformuje Twoją aplikację o tym, że pliki z rozszerzeniem .json powinny być traktowane jako moduły.
