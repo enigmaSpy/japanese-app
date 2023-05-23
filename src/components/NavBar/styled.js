9c39dc5c2d036076a8bf050a527bc9a1b49d34e5
enigmaSpy <soczekv18@gmail.com>
2023-05-23 16:23:18 +0200

    ui correction

--- src/components/NavBar/styled.js
+++ src/components/NavBar/styled.js
@@ -9,15 +9,20 @@ export const Nav = styled.nav`
 `;
 export const NavList = styled.ul`
     display: flex;
-    justify-content: space-evenly;
     align-items: center;
     width: 100%;
     margin: 15px 0 ;
+    @media (max-width: 768px) {
+      justify-content: space-evenly;
+    }
   `;
 export const ListItem = styled.li`
     list-style: none;
     display: inline-block;
-  
+    margin: 0 25px;
+    @media (max-width: 768px) {
+      margin: 0;
+    }
 `;
 
 export const StyledNavLink = styled(NavLink)`
