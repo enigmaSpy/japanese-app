b6f3bd028e53bc4f26adfd50bc58b8ff0462fe38
enigmaSpy <soczekv18@gmail.com>
2023-05-26 16:59:04 +0200

    code update

--- src/pages/QuizPage/index.js
+++ src/pages/QuizPage/index.js
@@ -1,98 +1,106 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
-import useAlphabet from "../../useAlphabet";
-
+import useQuiz from "../../useQuiz";
 import {
-    Character,
-    Input,
-    InputContainer,
-    NextButton,
-    QuizContainer,
-    QuizForm,
-    QuizTitle,
-    SubmitButton,
-    CharsLeft,
-    Allert
+  Character,
+  Input,
+  InputContainer,
+  NextButton,
+  QuizContainer,
+  QuizForm,
+  QuizTitle,
+  SubmitButton,
+  CharsLeft,
+  Allert
 } from "./styled";
 
-
 const QuizPage = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [quizAlphabet, removeQuizChar, resetQuiz] = useQuiz();
+  const [anwser, setAnwser] = useState("");
+  const [isCorrect, setIsCorrect] = useState(true);
+  const alphabet = quizAlphabet(id);
+  const [randomCharIndex, setRandomCharIndex] = useState(Math.floor(Math.random() * alphabet.length));
+  const inputRef = useRef(null);
+  const buttonRef = useRef(null);
 
-    const [quizAlphabet, removeChar, resetQuiz] = useAlphabet();
-    const [anwser, setAnwser] = useState("");
-    const [randomChar, setRandomChar] = useState(Math.floor(Math.random() * quizAlphabet(id).length));
-    const [isCorrect, setIsCorrect] = useState(true);
-    const [alphabet, setAlphabet] = useState(quizAlphabet(id));
-    const inputRef = useRef(null);
-    const buttonRef = useRef(null);
+  useEffect(() => {
+    if (alphabet.length > 0) {
+      inputRef.current.focus();
+    }
+    buttonRef.current.focus();
+  }, [alphabet.length]);
 
-
-    useEffect(() => {
-        setAlphabet(quizAlphabet(id));
-        if (alphabet.length > 0) {
-            inputRef.current.focus();
-        }
-        buttonRef.current.focus();
-
-    }, [id, quizAlphabet, alphabet.length]);
-
-    const handleCheck = (e) => {
-        e.preventDefault();
-        if (alphabet.length > 0) {
-            if (anwser.toLocaleLowerCase() === alphabet[randomChar].roumaji) {
-                setIsCorrect(true);
-                removeChar(id, alphabet[randomChar].id);
-                setRandomChar(Math.floor(Math.random() * alphabet.length));
-                setAnwser("");
-            } else {
-                setIsCorrect(false);
-            }
-        }
-    };
-
-    const handleNext = (e) => {
-        e.preventDefault();
+  const handleCheck = (e) => {
+    e.preventDefault();
+    if (alphabet.length > 0) {
+      const currentChar = alphabet[randomCharIndex];
+      if (anwser.toLocaleLowerCase() === currentChar?.roumaji) {
         setIsCorrect(true);
-        setTimeout(() => {
-            setRandomChar(Math.floor(Math.random() * alphabet.length))
-        }, 150);
+        removeQuizChar(id, currentChar.id); setRandomCharIndex(Math.floor(Math.random() * alphabet.length));
         setAnwser("");
+      } else {
+        setIsCorrect(false);
+      }
+    }
+  };
 
-    };
+  const handleNext = (e) => {
+    e.preventDefault();
+    setIsCorrect(true);
+    setTimeout(() => {
+      setRandomCharIndex(Math.floor(Math.random() * alphabet.length));
+    }, 100);
+    setAnwser("");
+  };
 
-    return (
+  useEffect(() => {
+    if (alphabet[randomCharIndex]?.roumaji === undefined) {
+      setRandomCharIndex(Math.floor(Math.random() * alphabet.length));
+    }
+  }, [randomCharIndex, alphabet]);
+
+  return (
+    <>
+      {alphabet.length > 0 ? (
         <>
-            {alphabet.length > 0 ? (
-                <>
-                    <QuizTitle>Quiz: {id}</QuizTitle>
-
-                    <CharsLeft>Zostało: {alphabet.length} znaków</CharsLeft>
-                    <Allert isCorrect={!isCorrect}>
-                        poprawna odpowiedź: {alphabet[randomChar].roumaji}
-                    </Allert>
-                    <QuizContainer>
-                        <Character>{alphabet[randomChar].kana}</Character>
-                    </QuizContainer>
-
-                    <QuizForm>
-                        <InputContainer isCorrect={!isCorrect}>
-                            <Input type="text" ref={inputRef} value={anwser} onChange={({ target }) => setAnwser(target.value)} />
-
-                            <SubmitButton onClick={handleCheck}>Check</SubmitButton>
-
-                        </InputContainer>
-                        <NextButton ref={buttonRef} isCorrect={!isCorrect} onClick={handleNext}>Next</NextButton>
-                    </QuizForm>
-                </>
-                ):(
-                    <>
-                        <QuizTitle>Gratulacje! Znasz już wszystkie znaki!</QuizTitle>
-                        <NextButton ref={buttonRef} reset onClick={() => resetQuiz(id)}>Reset</NextButton>
-                    </>
-                )}
+          <QuizTitle>Quiz: {id}</QuizTitle>
+          <CharsLeft>Zostało: {alphabet.length} znaków</CharsLeft>
+          <Allert isCorrect={!isCorrect}>
+            poprawna odpowiedź: {alphabet[randomCharIndex]?.roumaji}
+          </Allert>
+          <QuizContainer>
+            <Character>{alphabet[randomCharIndex]?.kana}</Character>
+          </QuizContainer>
+          <QuizForm>
+            <InputContainer isCorrect={!isCorrect}>
+              <Input
+                type="text"
+                ref={inputRef}
+                value={anwser}
+                onChange={({ target }) => setAnwser(target.value)}
+              />
+              <SubmitButton onClick={handleCheck}>Check</SubmitButton>
+            </InputContainer>
+            <NextButton
+              ref={buttonRef}
+              isCorrect={!isCorrect}
+              onClick={handleNext}
+            >
+              Next
+            </NextButton>
+          </QuizForm>
         </>
-    );
+      ) : (
+        <>
+          <QuizTitle>Gratulacje! Znasz już wszystkie znaki!</QuizTitle>
+          <NextButton ref={buttonRef} reset onClick={() => resetQuiz(id)}>
+            Reset
+          </NextButton>
+        </>
+      )}
+    </>
+  );
 };
 
 export default QuizPage;
