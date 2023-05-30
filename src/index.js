4bfc05ff98df965d98d00a73d892343e5e4a25ce
enigmaSpy <soczekv18@gmail.com>
2023-05-30 19:51:41 +0200

    testing service-worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { installOfflinePage, respondWithOfflinePage } from './service-worker';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,3 +16,15 @@ root.render(
   </React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.addEventListener('install', installOfflinePage);
+  navigator.serviceWorker.addEventListener('fetch', respondWithOfflinePage);
+
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered:', registration);
+    })
+    .catch(error => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
