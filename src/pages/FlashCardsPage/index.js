f74f136758773152f8491c80b71480e1e4f79f9d
enigmaSpy <soczekv18@gmail.com>
2023-05-30 14:52:48 +0200

    added interaction to flashcards

--- src/pages/FlashCardsPage/index.js
+++ src/pages/FlashCardsPage/index.js
@@ -1,32 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CardContainer, FrontCard, BackCard, ContentWrapper, CardContent, Buttons, Button } from "./styled";
+import useFlashCards from "../../useFlashCards";
+import { useParams } from "react-router-dom";
 
 const FlashCards = () => {
-
+    const { id } = useParams();
+    const [flashCardsAlphabet, removeChar, resetFlashCards] = useFlashCards();
     const [isFlipped, setIsFlipped] = useState(true);
+    const [charIndex, setCharIndex] = useState(0);
+    const [isEnd, setIsEnd] = useState(false);
+    const alphabet = flashCardsAlphabet(id);
+    
+
+    console.log(alphabet.length + " " + charIndex);
+
+    useEffect(() => {
+        if (alphabet.length === 0) {
+            setIsEnd(true);
+            
+        } else {
+            setIsEnd(false);
+        }
+    }, [alphabet]);
+
+    const handleKnow = () => {
+
+        removeChar(id, alphabet[charIndex].id);
+        if (alphabet.length > 1) {
+            if (charIndex === alphabet.length - 1) {
+                setCharIndex(0);
+            } 
+        }
+    };
+
+    const handleNotKnow = () => {
+        if(alphabet.length > 1){
+            setCharIndex((charIndex + 1) % alphabet.length);
+        }
+    };
+
+    console.log(alphabet.length);
 
     return (
         <ContentWrapper>
-            <h1>Flash Cards</h1>
+            <h1>Flash Cards: {id}</h1>
 
-            <CardContainer isFlipped={isFlipped} onClick={()=>setIsFlipped(!isFlipped)}>
-               <CardContent>
-               <FrontCard >
-                    front
-                </FrontCard>
-                <BackCard >
-                    Back
-                </BackCard>
-               </CardContent>
-            </CardContainer>
+            {isEnd ? (
+                <Button onClick={() => resetFlashCards(id)}>
+                    Reset
+                </Button>
+            ) : (
+                <>
+                    <CardContainer isFlipped={isFlipped} onClick={() => setIsFlipped(!isFlipped)}>
+                        <CardContent>
+                            <FrontCard>
+                                {alphabet[charIndex]?.kana}
+                            </FrontCard>
+                            <BackCard>
+                                {alphabet[charIndex]?.roumaji}
+                            </BackCard>
+                        </CardContent>
+                    </CardContainer>
 
-            <Buttons>
-                <Button isFlipped={isFlipped} onMouseOver={()=>setIsFlipped(true)}>ok</Button>
-                <Button>not ok</Button>
-            </Buttons>
+                    <Buttons>
+                        <Button onClick={handleKnow}>ok</Button>
+                        <Button onClick={handleNotKnow}>not ok</Button>
+                    </Buttons>
+                </>
+            )}
         </ContentWrapper>
     );
-
 };
 
 export default FlashCards;
